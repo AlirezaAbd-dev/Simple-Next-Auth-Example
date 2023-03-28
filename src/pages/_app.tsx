c0e2eb7a5ec680a5b933f225934d0756ee111ba7
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-28 17:44:35 +0430

    commited

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-import {SessionProvider} from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-  <SessionProvider session={session}>
-    <Component {...pageProps} />
-  </SessionProvider>
-  )
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
